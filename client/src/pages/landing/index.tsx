343c88b2844ca58b24f274281e2eba1dc27e8d73
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-08-27 17:51:34 +0800

    landing page placeholder

---
+++ client/src/pages/landing/index.tsx
@@ -0,0 +1,16 @@
+export const Landing = () => {
+  return (
+    <div className="grid grid-cols-2 py-32 gap-7">
+      <div className="flex flex-col gap-5">
+        <h1 className="text-5xl font-extrabold">Lorem ipsum dolor sit amet.</h1>
+        <p className="font-semibold text-lg">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit,
+          iusto?
+        </p>
+      </div>
+      <div>
+        <p>image</p>
+      </div>
+    </div>
+  );
+};
